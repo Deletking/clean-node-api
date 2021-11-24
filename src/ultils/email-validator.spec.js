6f694ef770fa1524fce7387ca2c3649e1ace4e5c
Paulo Hericles <paulo.hericles@squadra.com.br>
2021-11-24 18:53:45 -0300

    ensure emailValidator calls validator with correct email

--- src/ultils/email-validator.spec.js
+++ src/ultils/email-validator.spec.js
@@ -5,17 +5,27 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   test('Shoud return true if validator returns true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isvalid('valid_email@email.com')
     expect(isEmailValid).toBe(true)
   })
 
   test('Shoud return false if validator returns false', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isvalid('invalid_email@email.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Shoud call vakudator with correct email', () => {
+    const sut = makeSut()
+    sut.isvalid('any_email@email.com')
+    expect(validator.email).toBe('any_email@email.com')
   })
 })
